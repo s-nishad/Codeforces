ff178862e8d0712cdbcdcdbb256af97057ab54ae
s-nishad <shohanurnishad@gmail.com>
2021-10-19 20:12:39 +0600

    281A - Word Capitalization

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//  Created on
+//  281A - Word Capitalization
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[0] >= 97 && s[0] <= 122) {
+            s[0] -= 32;
+        }
+    }
+    cout <<  s;
+    return 0;
+}
