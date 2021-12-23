51468b164e1124d5fe28930bad49722cc2794082
Shohanur Nishad <60940498+s-nishad@users.noreply.github.com>
2021-12-24 01:50:06 +0600

    Create 58A Chat room.cpp

---
+++ 58A Chat room.cpp
@@ -0,0 +1,24 @@
+//
+// 58A. Chat room
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    string a = "hello";
+    int x = 0, y = 0;
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] == a[x]) {
+            x++;
+            y++;
+        }
+    }
+    if (y == 5) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
