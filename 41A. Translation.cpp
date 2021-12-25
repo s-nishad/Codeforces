b434f2d11df74389a3e6ac8924558c361db5508f
Shohanur Nishad <60940498+s-nishad@users.noreply.github.com>
2021-12-26 01:28:13 +0600

    Create 41A. Translation.cpp

---
+++ 41A. Translation.cpp
@@ -0,0 +1,17 @@
+//
+// 41A. Translation
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+    reverse(t.begin(), t.end());
+    if (s == t) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
