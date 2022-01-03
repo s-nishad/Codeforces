4236935f885d85fb172085d45de63d04d593e1f7
Shohanur Nishad <60940498+s-nishad@users.noreply.github.com>
2022-01-04 00:38:43 +0600

    Create 136A. Presents.cpp

---
+++ 136A. Presents.cpp
@@ -0,0 +1,19 @@
+//
+// 136A. Presents
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, p;
+    cin >> n;
+    int ans[n + 1];
+    for (int i = 1; i <= n; ++i) {
+        cin >> p;
+        ans[p] = i;
+    }
+    for (int i = 1; i <= n; ++i) {
+        cout << ans[i] << " ";
+    }
+}
